9af841ae493b779943875052759c43d08c093467
Muhammed Enes Karaca <eneskaracap@gmail.com>
2025-12-07 10:02:02 +0300

    added safety checks and constructor method comment

--- ImplementationAssigment2/src/Battery.cpp
+++ ImplementationAssigment2/src/Battery.cpp
@@ -1,60 +1,92 @@
 #include "Battery.h"
 #include <iostream>
 
+Battery::Battery(double v, double c, double initialCharge)
+{
+    if (v < 0)
+    {
+        v = 0;
+    }
 
-Battery::Battery(double v, double c, double initialCharge){
+    if (c < 0)
+    {
+        c = 0;
+    }
+
     voltage = v;
     capacity = c;
 
-    if(initialCharge < 0){
+    if (initialCharge < 0)
+    {
         charge = 0;
     }
-    else if(initialCharge > capacity){
+    else if (initialCharge > capacity)
+    {
         charge = capacity;
     }
-    else{
+    else
+    {
         charge = initialCharge;
     }
-
 }
 
-void Battery::use(double hours){
+void Battery::use(double hours)
+{
+    // if negative hours given, do nothing
+    if (hours < 0)
+    {
+        return;
+    }
 
-    charge = charge - hours*DISCHARGE_RATE;
+    charge = charge - hours * DISCHARGE_RATE;
 
-    if(charge < 0){
+    if (charge < 0)
+    {
         charge = 0;
     }
 }
 
-void Battery::recharge(double hours){
-    charge = charge + hours*RECHARGE_RATE;
+void Battery::recharge(double hours)
+{
+    // if negative hours given, do nothing
+    if (hours < 0)
+    {
+        return;
+    }
 
-    if(charge > capacity){
+    charge = charge + hours * RECHARGE_RATE;
+
+    if (charge > capacity)
+    {
         charge = capacity;
     }
 }
 
-double Battery::getVoltage() const {
-    return voltage; 
+double Battery::getVoltage() const
+{
+    return voltage;
 }
 
-double Battery::getCapacity() const {
-    return capacity; 
+double Battery::getCapacity() const
+{
+    return capacity;
 }
 
-double Battery::getCharge() const {
-    return charge; 
+double Battery::getCharge() const
+{
+    return charge;
 }
 
-double Battery::getPercent() const {
-    
-    //for secure code
-    if (capacity == 0) return 0;
-    
-    return (charge/capacity)*100; 
+double Battery::getPercent() const
+{
+
+    // for secure code
+    if (capacity == 0)
+        return 0;
+
+    return (charge / capacity) * 100;
 }
 
-Battery::~Battery(){
-    
+Battery::~Battery()
+{
 }
