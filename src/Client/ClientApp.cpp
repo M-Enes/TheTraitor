896da1118f1c5a4fd465825bbda4c05cc64ef42c
theHapalua <23050111060@aybu.edu.tr>
2025-12-24 16:29:06 +0300

    Default port is updated

--- src/Client/ClientApp.cpp
+++ src/Client/ClientApp.cpp
@@ -13,7 +13,7 @@ namespace TheTraitor {
 		: window(sf::VideoMode::getFullscreenModes()[0], "The Traitor", sf::Style::None, sf::State::Fullscreen),
 		inputHandler(window),
 		gameView(window, executableFolderPath),
-		serverPort(5000),
+		serverPort(53000),
 		serverIp(sf::IpAddress::LocalHost),
 		menuMusic(executableFolderPath + "/assets/music/enchantedtiki86.mp3"),
 		actionPhaseMusic(executableFolderPath + "/assets/music/battleThemeB.mp3"),
